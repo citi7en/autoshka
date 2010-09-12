c82925af53c4e16d142f8abbe07ba26f63c35266
citi7en <ryakhov.ivan@gmail.com>
2010-09-12 17:29:45 +0400

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+		@title = "Свежий номер"
   end
 
   def contact
+		@title = "Контактная информация"
   end
+
+	def about
+		@title = "О сайте"
+	end
 
 end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe PagesController do
+	integrate_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+		it "should have the right title" do
+			get 'home'
+			response.should have_tag("title", 
+															 "Автошка online | Свежий номер")
+		end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+		it "should have the right title" do
+			get 'contact'
+			response.should have_tag("title",
+															 "Автошка online | Контактная информация")
+		end
+  end
+
+	describe "GET 'about'" do
+		it "should be successful" do
+			get 'about'
+			response.should be_success
+		end
+
+		it "should have the right title" do
+			get 'about'
+			response.should have_tag("title",
+															 "Автошка online | О сайте")
+		end
+	end
+end
