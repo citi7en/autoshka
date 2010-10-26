cdba5ef0c13fceefdd7905ee2c10b19423488d90
citi7en <ryakhov.ivan@gmail.com>
2010-10-26 12:20:04 +0400

    Revert "russian plugin introducing"
    This reverts commit dfe99b8f178ffd324ced1436c391248d9a08db1b.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,36 +17,38 @@ class User < ActiveRecord::Base
 
 	EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
-	validates_presence_of :name 
-	validates_presence_of :email
-	validates_length_of		:name, :maximum => 20
-	validates_format_of		:email, :with => EmailRegex
+	validates_presence_of :name, 
+												:message => 'должно быть введено.'
+	validates_presence_of :email,
+												:message => 'должен быть введён.'
+	validates_length_of		:name, :maximum => 20,
+												:message => 'должно не больше 20 символов'
+	validates_format_of		:email, :with => EmailRegex,
+												:message => 'имеет неправильный формат.'
 
 	validates_uniqueness_of	:name, :case_sensitive => false,
-													:message => "уже использовано."
+													:message => 'уже использовано.'
 	validates_uniqueness_of :email, :case_sensitive => false,
-													:message => "уже использован."
+													:message => 'уже использован.'
 	# Automatically create the virtual attribute 'password_confirmation'.
 	validates_confirmation_of	:password,
 							:message => 'должен совпадать с полем для подтверждения.'
 
 	# Password validations.
 	validates_presence_of :password,
-												:message => "должен быть введён."
+												:message => 'должен быть введён.'
 	validates_length_of		:password, :within => 6..40,
-												:message => "должен быть от 6 до 40 символов."
+												:message => 'должен быть от 6 до 40 символов.'
 
-=begin
 	HUMAN_ATTRIBUTES = {
-#			:name => "Имя пользователя",
+			:name => "Имя пользователя",
 			:email => "Адрес электронной почты",
 			:password => "Пароль"
 		}
 
 	def self.human_attribute_name(attr)
 		HUMAN_ATTRIBUTES[attr.to_sym] || super
-	end 
-=end
+	end
 
 
 	before_save :encrypt_password
@@ -65,7 +67,7 @@ class User < ActiveRecord::Base
   private
 
     def encrypt_password
-			self.salt = make_salt
+      self.salt = make_salt
 			self.encrypted_password = encrypt(password)
     end
 
